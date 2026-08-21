ccda567745ef96ed6b8f27ac7fb6600d18f3584e
Advait Saxena <saxenaadvait23@gmail.com>
2026-08-21 18:45:03 +0530

    Add Experiment1.c for character handling example

---
+++ Week3/0.3.1/Experiment1.c
@@ -0,0 +1,11 @@
+// 0.3.1.1 Passing and Returning Integer Using Function
+#include<stdio.h>
+char GetChar(char ch){
+    return ch;
+}
+int main(){
+    char c='a';
+    printf("Advait\tSaxena\n251090580220\nRno:66\n");
+    printf("The returned character is: %c",GetChar(c));
+    return 0;
+}
